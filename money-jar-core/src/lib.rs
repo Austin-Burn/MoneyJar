f36ed9cefe0dc42d80ab5c3dbcdbef95eca54b98
Erisdar Skolir <erisdarskolir@gmail.com>
2025-05-08 20:50:25 -0400

    Scaffold

---
+++ money-jar-core/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn hello(name: String) -> String {
+    format!("Hello, {}!", name)
+}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
